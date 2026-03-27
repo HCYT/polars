e043c9da499829314d805e7e7e90df8b3ba8af05
Simon Lin <simonlin.rqmmw@slmail.me>
2026-03-28 04:47:53 +1100

    c

--- crates/polars-plan/src/plans/optimizer/simplify_ordering/mod.rs
+++ crates/polars-plan/src/plans/optimizer/simplify_ordering/mod.rs
@@ -336,6 +336,17 @@ impl SimplifyIRNodeOrder<'_> {
 
                 #[cfg(feature = "asof_join")]
                 if let polars_ops::prelude::JoinType::AsOf(_) = &options.args.how {
+                    if out_edge.is_unordered()
+                        && (in_edge_lhs.is_unordered() || in_edge_rhs.is_unordered())
+                    {
+                        *in_edge_lhs = Edge::Unordered;
+                        *in_edge_rhs = Edge::Unordered;
+                    }
+
+                    if in_edge_lhs.is_unordered() && in_edge_rhs.is_unordered() {
+                        *out_edge = Edge::Unordered;
+                    }
+
                     return false;
                 }
 
